5f8578f9430d3e76dc9ee2d9c02bbb9a8dc2fdcf
StrangeQuark <zacharyzirkle@hotmail.com>
2025-09-28 10:24:18 -0500

    Add github button to ServiceContainers, update media CSS

--- src/components/ServiceContainer.jsx
+++ src/components/ServiceContainer.jsx
@@ -1,6 +1,14 @@
 import "./css/ServiceContainer.css";
+import { FaGithub } from "react-icons/fa";
 
 const ServiceContainer = ({ item, toggleItem }) => {
+  const githubURL = "https://github.com/StrangeQuark/"
+
+  const handleGithubClick = (e) => {
+    e.stopPropagation(); // prevent triggering toggleItem
+    window.open(githubURL + item.repo, "_blank");
+  };
+  
   return (
     <div
       id="service-container"
@@ -8,6 +16,13 @@ const ServiceContainer = ({ item, toggleItem }) => {
       onClick={toggleItem}
     >
       <h1 className={`container-text ${item.isSelected ? "container-text-selected" : ""}`}>{item.name}</h1>
+
+      {/* GitHub Button */}
+      {item.repo && (
+        <div className="github-button-container" onClick={handleGithubClick}>
+          <FaGithub className="github-icon" />
+        </div>
+      )}
     </div>
   );
 };
